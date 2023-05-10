800254d36fbbf0b70670b7d19f149a4c8a4be06d
frannpor <frannporciel@gmail.com>
2023-05-10 20:24:09 -0300

    server-config-almost-done

---
+++ api/src/models/Dog.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID, //* El id se genera automáticamente si no lo ponemos
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.STRING,
+    },
+  },
+    { timestamps: false });
+
+};
