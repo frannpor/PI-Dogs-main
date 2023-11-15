6f159a5513e889bdee3b0442ac2b71d6dcdebe17
Francisco Porciel <Francisco>
2023-11-15 19:11:33 -0300

    Removed localhost to CORS

--- api/src/app.js
+++ api/src/app.js
@@ -22,7 +22,7 @@ server.use(cors({
   credentials: true,
 }));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', ['http://localhost:3000', SERVER_HOST]);
+  res.header('Access-Control-Allow-Origin', [SERVER_HOST]);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
